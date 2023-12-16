0b6ec0bc89cbb8f734584d728ba5b20bc127ea7c
Arman Bhatia <arman.bhatia.1407@gmail.com>
2023-12-15 19:24:07 -0500

    Theme customization completed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    screens: {
+      "sm": "640px",
+      "md": "768px",
+      "lg": "1024px",
+      "xl": "1280px",
+      "2xl": "1536px",
+    },
+    extend: {
+      colors: {
+        transparent: "transparent",
+        current: "currentColor",
+        "violet": "#9e7f66",
+        "light-violet": "#939BF4",
+        "very-dark-blue": "#19202D",
+        "midnight": "#121721",
+        "white": "#ffffff",
+        "light-Grey": "#ecebff",
+        "gray": "#ff77e9",
+        "dark-grey": "#6E8098",
+      },
+      fontFamily: {
+        sans: ["Kumbh Sans", "sans-serif"],
+        serif: ["Merriweather", "serif"],
+      },
+    },
   },
   plugins: [],
-}
-
+};
