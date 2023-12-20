fdc114e40e6dc33bc83a6bb658228714c69c7ad6
Arman Bhatia <arman.bhatia.1407@gmail.com>
2023-12-20 02:44:23 -0500

    mobile version completed, theme switching left

--- src/component/UI/Header.jsx
+++ src/component/UI/Header.jsx
@@ -2,13 +2,17 @@ import Brightness7Icon from "@mui/icons-material/Brightness7";
 import Brightness3Icon from "@mui/icons-material/Brightness3";
 import Switch from "@mui/material/Switch";
 
+import { Link } from "react-router-dom";
+
 function Header() {
   return (
     <header className="bg-mobile-header p-5 pt-8 h-32 rounded-bl-3xl">
       <div className="flex justify-between items-center ">
-        <div>
-          <img src="./assets/desktop/logo.svg" alt="Logo-Devjobs" className=""></img>
-        </div>
+        <Link to="/">
+          <div>
+            <img src="./assets/desktop/logo.svg" alt="Logo-Devjobs" className=""></img>
+          </div>
+        </Link>
         <div className="theme-action-btn">
           <Brightness7Icon sx={{ color: "white" }} />
           <Switch onChange={() => {}} inputProps={{ "aria-label": "controlled" }} color="default" />
