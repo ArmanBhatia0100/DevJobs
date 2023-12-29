284e80cef39b71a64a1f080922873bd693527847
Arman Bhatia <arman.bhatia.1407@gmail.com>
2023-12-28 23:33:19 -0500

    minor fix

--- src/component/Auth/Auth.jsx
+++ src/component/Auth/Auth.jsx
@@ -3,7 +3,7 @@ import { addUser } from "../../util/firebase";
 import { useNavigate } from "react-router-dom";
 import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
 
-import { TextField, Button } from "@mui/material";
+import { TextField } from "@mui/material";
 import { useState } from "react";
 
 const Auth = () => {
@@ -76,7 +76,7 @@ const Auth = () => {
               }}
             />
             {loginLoading ? (
-              <button type="button" class="text-white bg-[#1976d2] dark:bg-[#1976d2] cursor-not-allowed font-semibold rounded-lg  text-lg px-5 py-2.5 text-center" disabled>
+              <button type="submit" class="text-white bg-[#1976d2] dark:bg-[#1976d2] cursor-not-allowed font-semibold rounded-lg  text-lg px-5 py-2.5 text-center" disabled>
                 <svg aria-hidden="true" role="status" class="inline w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
                   <path
                     d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
@@ -90,7 +90,7 @@ const Auth = () => {
                 Login
               </button>
             ) : (
-              <button type="button" class="text-white bg-[#1976d2] dark:bg-[#1976d2] cursor-not-allowed font-semibold rounded-lg  text-lg px-5 py-2.5 text-center" disabled>
+              <button type="submit" class="text-white bg-[#1976d2] dark:bg-[#1976d2]  font-semibold rounded-lg  text-lg px-5 py-2.5 text-center">
                 Login
               </button>
             )}
